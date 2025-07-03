e7255892a819e7f2c56c6eb67b06dc100c7c2250
Aiteam55 <mitia.andriandimbihery@pulse.mg>
2025-07-03 16:52:14 +0300

    implement test for registering an app

--- src/test/java/com/aiteam/gatekeeper/services/ClientAppServiceTest.java
+++ src/test/java/com/aiteam/gatekeeper/services/ClientAppServiceTest.java
@@ -1,25 +1,28 @@
 package com.aiteam.gatekeeper.services;
 
 import com.aiteam.gatekeeper.dtos.requests.ClientAppRequestDTO;
+import com.aiteam.gatekeeper.dtos.responses.ClientAppResponseDTO;
+import com.aiteam.gatekeeper.entities.ClientApp;
 import com.aiteam.gatekeeper.repositories.ClientAppRepository;
 import com.aiteam.gatekeeper.services.impl.ClientAppServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class ClientAppServiceTest {
-    private IClientAppService clientAppService;
+    @Mock
     private ClientAppRepository clientAppRepository;
 
-    @BeforeEach
-    public void setup() {
-        clientAppService = new ClientAppServiceImpl(clientAppRepository);
-    }
+    @InjectMocks
+    private ClientAppServiceImpl clientAppService;
 
     @Test
     void testConnectionShouldReturnTrueWithValidCredentials() {
@@ -33,5 +36,30 @@ public class ClientAppServiceTest {
         ClientAppRequestDTO clientAppRequestDTO = new ClientAppRequestDTO("Client App", "localhost", "3306", "wrongdb", "root", "root");
         boolean result = clientAppService.testConnection(clientAppRequestDTO);
         assertFalse(result, "Expected valid DB connection to return false");
+    }
+
+    @Test
+    void shouldRegisterClientAppSuccessfully() {
+        // Given
+        ClientApp clientApp = new ClientApp();
+        clientApp.setName("ClientTest");
+        clientApp.setDbHost("localhost");
+        clientApp.setDbPort("3306");
+        clientApp.setDbName("testdb");
+        clientApp.setDbUsername("root");
+        clientApp.setDbPassword("secret");
+
+        ClientAppRequestDTO dto = new ClientAppRequestDTO(
+                "ClientTest", "localhost", "3306", "testdb", "root", "secret"
+        );
+
+        when(clientAppRepository.save(any(ClientApp.class))).thenReturn(clientApp);
+
+        // When
+        ClientAppResponseDTO result = clientAppService.registerClientApp(dto);
+
+        // Then
+        assertEquals("ClientTest", result.name());
+        verify(clientAppRepository).save(any(ClientApp.class));
     }
 }
